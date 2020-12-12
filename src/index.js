e5cbdbab34de32dc25a5fa0cdc7c3ea76edb8a95
nikhilDevarasetty <nicholus158@gmail.com>
2020-12-12 17:57:14 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connections } = require('mongoose');
+const { connections } = require("mongoose");
 
+const { connection } = require("./connector");
 
+app.get("/findColleges", (req, res) => {
+  connection
+    .find(req.query)
+    .then((data) => res.send(data))
+    .catch((err) => res.send(err.message));
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
